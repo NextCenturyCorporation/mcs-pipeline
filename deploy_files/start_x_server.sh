3c12a39594d3b3102dd1c7f6f1f509cd31d6dd33
clark dorman <clarkus_dormanus@hotmail.com>
2021-07-06 08:39:41 -0400

    Handle std output and std error

--- deploy_files/start_x_server.sh
+++ deploy_files/start_x_server.sh
@@ -8,7 +8,7 @@ if [ -z $XPROCESS ]; then
 else
   echo "Starting X Server"
   sudo nvidia-xconfig --use-display-device=None --virtual=1280x1024 --output-xconfig=/etc/X11/xorg.conf --busid=PCI:0:30:0
-  sudo /usr/bin/Xorg :0 &
+  sudo /usr/bin/Xorg :0 1>startx-out.txt 2>startx-err.txt &
   echo "Sleeping for 20 seconds to wait for X server"
   sleep 20
   echo "Sleep finished"
@@ -18,17 +18,6 @@ else
     echo 'Successfully started X Server'
   else
     echo 'Error:  Unable to start X Server!!'
+    exit 1
   fi
 fi
-
-
-
-
-
-
-
-# Start X
-# TODO:  Check to see if the xserver is already running and do not restart
-cd mcs-pipeline/xserver
-sudo nohup python3 run_startx.py 1>startx-out.txt 2>startx-err.txt &
-sleep 20
