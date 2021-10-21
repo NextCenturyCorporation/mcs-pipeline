009d1276271425b1f1a62497b82d098e82db5834
deanwetherby <dean.wetherby@ieee.org>
2021-10-21 09:14:07 -0400

    pep8 formatting

--- pipeline/time_utils.py
+++ pipeline/time_utils.py
@@ -3,5 +3,4 @@ import time
 
 def get_date_in_file_format():
     """Get the date in a format like 2020-03-01, useful for creating files"""
-    time_in_file_format = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-    return time_in_file_format
+    return time.strftime('%Y-%m-%d', time.localtime(time.time()))
