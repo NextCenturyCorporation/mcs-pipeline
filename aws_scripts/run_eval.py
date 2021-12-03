06b34cf2e0c6008993f7cbb2c7695da085f3e743
Kyle Drumm <kyle.drumm@caci.com>
2021-12-03 09:40:46 -0500

    prototype for running an entire eval from a single file

--- aws_scripts/run_eval.py
+++ aws_scripts/run_eval.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from dataclasses import dataclass, field
 
+import copy
 import pathlib
+import subprocess
 from typing import Dict, List
 
+import pathlib
 import yaml
 import os
 import threading
@@ -31,15 +34,23 @@ def add_variable_sets(varsets):
     return vars
 
 
-def execute_shell(cmd):
+def execute_shell(cmd, log_file=None):
     # By passing all the commands into this function, the method of
     # executing the shell can easily be changed later.  This could be useful
     # if we want to capture the logging.
-    os.system(cmd)
+    cmd = f"{cmd} 2>&1 | ts -S"
+    if (log_file):
+        with open(log_file, "a") as f:
+            subprocess.run([cmd, "|", "ts"], stdout=f,
+                           stderr=subprocess.STDOUT, shell=True)
+    else:
+        subprocess.run(cmd, shell=True)
+
+    # os.system(cmd)
 
 
 def run_eval(varset, local_scene_dir, metadata="level2", disable_validation=False,
-             dev_validation=False, resume=False, override_params={}):
+             dev_validation=False, resume=False, override_params={}, log_file=None):
     # Get Variables
     vars = add_variable_sets(varset)
     vars = {**vars, **override_params}
@@ -66,7 +77,7 @@ def run_eval(varset, local_scene_dir, metadata="level2", disable_validation=Fals
     # Create config file
     # metadata level
     cmd = f'ray up -y {ray_cfg_file.as_posix()}'
-    execute_shell(cmd)
+    execute_shell(cmd, log_file)
 
     # Ray Start
     # ray up -y $RAY_CONFIG
@@ -77,11 +88,12 @@ def run_eval(varset, local_scene_dir, metadata="level2", disable_validation=Fals
     # ray rsync_up -v $RAY_CONFIG pipeline '~'
     # ray rsync_up -v $RAY_CONFIG deploy_files/${MODULE}/ '~'
     # ray rsync_up -v $RAY_CONFIG configs/ '~/configs/'
-    execute_shell(f"ray rsync_up -v {ray_cfg_file.as_posix()} pipeline '~'")
     execute_shell(
-        f"ray rsync_up -v {ray_cfg_file.as_posix()} deploy_files/{team}/ '~'")
+        f"ray rsync_up -v {ray_cfg_file.as_posix()} pipeline '~'", log_file)
     execute_shell(
-        f"ray rsync_up -v {ray_cfg_file.as_posix()} configs/ '~/configs/'")
+        f"ray rsync_up -v {ray_cfg_file.as_posix()} deploy_files/{team}/ '~'", log_file)
+    execute_shell(
+        f"ray rsync_up -v {ray_cfg_file.as_posix()} configs/ '~/configs/'", log_file)
 
     # Find and read Ray locations config file
     # source aws_scripts/load_ini.sh $RAY_LOCATIONS_CONFIG
@@ -103,7 +115,7 @@ def run_eval(varset, local_scene_dir, metadata="level2", disable_validation=Fals
 
     # ray exec $RAY_CONFIG "mkdir -p $MCS_scene_location"
     execute_shell(
-        f'ray exec {ray_cfg_file.as_posix()} "mkdir -p {remote_scene_location}"')
+        f'ray exec {ray_cfg_file.as_posix()} "mkdir -p {remote_scene_location}"', log_file)
 
     # this may cause re-used machines to have more scenes than necessary in the follow location.
     # I believe this is ok since we use the txt file to control exactly which files are run.
@@ -112,10 +124,10 @@ def run_eval(varset, local_scene_dir, metadata="level2", disable_validation=Fals
     # ray rsync_up -v $RAY_CONFIG $TMP_DIR/scenes_single_scene.txt "$MCS_scene_list"
     execute_shell(
         f'ray rsync_up -v {ray_cfg_file.as_posix()} '
-        f'{local_scene_dir}/ "{remote_scene_location}"')
+        f'{local_scene_dir}/ "{remote_scene_location}"', log_file)
     execute_shell(
         f'ray rsync_up -v {ray_cfg_file.as_posix()} '
-        f'{scene_list_file.as_posix()} "{remote_scene_list}"')
+        f'{scene_list_file.as_posix()} "{remote_scene_list}"', log_file)
 
     submit_params = "--disable_validation" if disable_validation else ""
     submit_params += " --resume" if resume else ""
@@ -124,7 +136,7 @@ def run_eval(varset, local_scene_dir, metadata="level2", disable_validation=Fals
     # ray submit $RAY_CONFIG pipeline_ray.py $RAY_LOCATIONS_CONFIG $MCS_CONFIG $SUBMIT_PARAMS
     execute_shell(
         f"ray submit {ray_cfg_file.as_posix()} pipeline_ray.py "
-        f"{ray_locations_config} {mcs_config} {submit_params}")
+        f"{ray_locations_config} {mcs_config} {submit_params}", log_file)
 
 
 @dataclass
@@ -139,11 +151,12 @@ def create_eval_set_from_folder(varset: List[str], base_dir: str, metadata: str 
     eval_set = []
     dirs = os.listdir(base_dir)
     for dir in dirs:
+        my_override = copy.deepcopy(override)
         scene_dir = os.path.join(base_dir, dir)
         if os.path.isdir(scene_dir):
-            override["log_name"] = f"{dir}-{metadata}.log"
+            my_override["log_name"] = f"{dir}-{metadata}.log"
             eval_set.append(EvalParams(varset, scene_dir,
-                            metadata=metadata, override=override))
+                            metadata=metadata, override=my_override))
     return eval_set
 
 
@@ -153,14 +166,25 @@ def run_evals(eval_set: List[EvalParams], num_clusters=3):
         q.put(eval)
 
     def run_eval_from_queue(num):
+        log_dir_path = "logs-test"
+        log_dir = pathlib.Path(log_dir_path)
+        log_dir.mkdir(parents=True, exist_ok=True)
         while(not q.empty()):
             eval = q.get()
             override = eval.override
             override["clusterSuffix"] = f"-{num}"
-            # run_eval(eval.varset, eval.scene_dir, eval.metadata,
-            #         override_params=eval.override)
             print(f"Starting eval from {eval.scene_dir} in cluster {num}")
-            time.sleep(random.randint(4, 20))
+
+            log_file_name = override.get("log_name")
+            if log_file_name:
+                log_file = log_dir / pathlib.Path(log_file_name)
+                log_file.unlink(missing_ok=True)
+            # run_eval(eval.varset, eval.scene_dir, eval.metadata,
+            #         override_params=eval.override, log_file=log_file)
+            execute_shell("echo Starting", log_file)
+            execute_shell(
+                f"sleep {random.randint(1, 5)}; echo sleeping", log_file)
+            execute_shell("echo Finishing", log_file)
             print(f"Finished eval from {eval.scene_dir} in cluster {num}")
         print(f"Finished with cluster {num}")
 
@@ -174,11 +198,57 @@ def run_evals(eval_set: List[EvalParams], num_clusters=3):
         t.join()
 
 
+def force_array(val):
+    return val if isinstance(val, list) else [val]
+
+
+def get_array(group, base, field):
+    return force_array(group.get(field, base.get(field, [])))
+
+
+def create_eval_set_from_file(cfg_file: str):
+    with open(cfg_file, 'r') as reader:
+        cfg = yaml.safe_load(reader)
+
+    base = cfg.get('base', {})
+
+    eval_groups = force_array(cfg.get('eval-groups', []))
+
+    evals = []
+
+    for group in eval_groups:
+        my_base = copy.deepcopy(base)
+        varset = get_array(group, my_base, 'varset')
+        metadata_list = get_array(group, my_base, 'metadata')
+        for metadata in metadata_list:
+            parents = get_array(group, my_base, 'parent-dir')
+            dirs = get_array(group, my_base, 'dirs')
+            for dir in dirs:
+                my_override = {}
+                log_dir = dir.split("/")[-1]
+                my_override["log_name"] = f"{log_dir}-{metadata}.log"
+                evals.append(EvalParams(
+                    varset, dir, metadata, override=my_override))
+            for parent in parents:
+                new_evals = create_eval_set_from_folder(
+                    varset, parent, metadata)
+                evals += new_evals
+    return evals
+
+
+def file_test():
+    cfg_file = "mako/eval.yaml"
+    test_set = create_eval_set_from_file(cfg_file)
+
+    run_evals(test_set)
+
+
 def multi_test():
     varset = ['opics', 'kdrumm']
     metadata = 'level2'
     base_dir = 'eval4/split'
     test_set = create_eval_set_from_folder(varset, base_dir, metadata)
+    # TODO allow test_sets be created in files
 
     run_evals(test_set)
 
@@ -195,4 +265,4 @@ def single_test():
 
 if __name__ == "__main__":
     # args = parse_args()
-    multi_test()
+    file_test()
