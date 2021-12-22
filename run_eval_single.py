8700000e02e61c9443cc32d71316afa7b9a3df97
Kyle Drumm <kyle.drumm@caci.com>
2021-12-22 12:39:59 -0500

    fixing single run script

--- run_eval_single.py
+++ run_eval_single.py
@@ -1,12 +1,21 @@
 import argparse
 
-from run_eval import EvalParams, get_now_str, run_eval
+from run_eval import EvalParams, execute_shell, get_now_str, EvalRun
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Run multiple eval sets containing scenes using ray.  There are two modes.  " +
-        "One uses the '--config_file' option.  The other uses the '--local_scene_dir' option.")
+        description="Run a single ray job from files from a single directory.")
+    parser.add_argument(
+        "--local_scene_dir", "-s",
+        default=None, required=True,
+        help="Local scene directory to be used for a single run.",
+    )
+    parser.add_argument(
+        "--local_template_dir", "-t",
+        default='mako',
+        help="Local directory containing 'variables' and 'templates' directory for creating config files.",
+    )
     parser.add_argument(
         "--dev_validation", "-d",
         default=False,
@@ -26,30 +35,30 @@ def parse_args():
         help="Whether or not to copy output logs to stdout",
     )
     parser.add_argument(
-        "--local_scene_dir", "-s",
-        default=None,
-        help="Local scene directory to be used for a single run.",
-    )
-    parser.add_argument(
         "--metadata", "-m",
         default='level2',
         help="Sets the metadata level for a single run.",
     )
+    # At one point, I had varset as required.  However, if the user wants to create config file
+    # without variables, they can use a template that is the config file itself without variables.
     parser.add_argument(
-        "--varset", "-v", type=str, nargs='+',
+        "--varset", "-v", type=str, nargs='+', default=[],
         help="Sets list of variable set files that should be read.",
     )
     return parser.parse_args()
 
 
-# TODO MCS-1066 Finish and test
 if __name__ == "__main__":
     args = parse_args()
     now = get_now_str()
     vars = "-".join(args.varset)
     log_file = f"logs/{now}-{vars}-{args.metadata}.log"
     ep = EvalParams(args.varset, args.local_scene_dir, args.metadata)
-    run_eval(ep.varset, ep.local_scene_dir, ep.metadata,
-             disable_validation=args.disable_validation,
-             dev_validation=args.dev_validation,
-             resume=args.resume, log_file=log_file, output_logs=args.redirect_logs)
+    execute_shell("echo Starting `date`", log_file)
+    run = EvalRun(ep, disable_validation=args.disable_validation,
+                  dev_validation=args.dev_validation,
+                  log_file=log_file,
+                  output_logs=args.redirect_logs)
+    run.run_eval()
+    execute_shell("echo Finishing `date`", log_file)
+    execute_shell(f"ray down -y {run.ray_cfg_file.as_posix()}", log_file)
