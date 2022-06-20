f6c5c2303489d0cde81377e9101eecf9fff1ea94
Kyle Drumm <kyle.drumm@caci.com>
2022-06-20 13:03:21 -0400

    updates to opics configs

--- deploy_files/opics/run_opics_commands.sh
+++ deploy_files/opics/run_opics_commands.sh
@@ -5,4 +5,4 @@ opics_eval5
 sudo /usr/bin/Xorg :0 &
 sudo nvidia-xconfig --use-display-device=None --virtual=600x400 --output-xconfig=/etc/X11/xorg.conf --busid=PCI:0:30:0
 echo $2
-cd "$1/scripts" && cp "$2" ../cfg/mcs_config.ini && python run_opics.py --scenes "$3"
+cd "$1" && cp "$2" ../cfg/mcs_config.ini && python run_opics.py --scenes "$3"
