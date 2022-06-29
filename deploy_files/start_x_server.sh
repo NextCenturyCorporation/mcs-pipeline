754920fe7233b5832cc30d8f37527fcde48cd9be
Brian Pippin <brian.pippin@caci.com>
2022-06-29 10:37:47 -0400

    Changes needed from collab_first_run to make the g4dn runs work.  Copied in some of Rachel's changes

--- deploy_files/start_x_server.sh
+++ deploy_files/start_x_server.sh
@@ -6,7 +6,16 @@ then
   echo 'X Server is running'
 else
   echo "Starting X Server"
-  sudo nvidia-xconfig --use-display-device=None --virtual=1280x1024 --output-xconfig=/etc/X11/xorg.conf --busid=PCI:0:30:0
+  #sudo nvidia-xconfig --use-display-device=None --virtual=1280x1024 --output-xconfig=/etc/X11/xorg.conf --busid=PCI:0:30:0
+  # Note that switching from the p2.xlarge machines to g4dn.xlarge means using a Tesla T4 card,
+  # instead of a K80. This is important because T4 doesn't support the --use-display-device=None
+  # option that used to be specified here.
+
+  # See "No Scanout Mode" here:
+  # https://docs.nvidia.com/datacenter/tesla/tesla-release-notes-440-3301/index.html#known-issues
+
+  sudo nvidia-xconfig --no-use-display-device --virtual=1280x1024 --output-xconfig=/etc/X11/xorg.conf --busid=PCI:0:30:0
+
   sudo /usr/bin/Xorg :0 1>startx-out.txt 2>startx-err.txt &
   echo "Sleeping for 20 seconds to wait for X server"
   sleep 20
