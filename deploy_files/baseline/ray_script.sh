a3fc05198fe2afaf7aeff1f19a9c154980b56c50
Thomas Schellenberg <thomas.schellenberg@caci.com>
2022-07-15 09:33:16 -0400

    Split NYU baseline pipeline configs into separate files for each eval task.

--- deploy_files/baseline/ray_script.sh
+++ deploy_files/baseline/ray_script.sh
@@ -9,51 +9,50 @@ source /home/ubuntu/check_passed_variables.sh
 # shellcheck disable=SC2154
 # SCENE_DIR="$eval_dir/scenes/"
 
-sudo apt install -y jq
 scene_name=$(cat $scene_file | jq --raw-output '.name')
-task_dir=""
-if [[ $scene_name == sierra_* ]]
-then
-  task_dir="agent_id";
-fi
-if [[ $scene_name == tango_* ]]
-then
-  task_dir="moving_target";
-fi
-if [[ $scene_name == uniform_* ]]
-then
-  task_dir="lava";
-fi
-if [[ $scene_name == victor_* ]]
-then
-  task_dir="lava";
-fi
-if [[ $scene_name == whiskey_* ]]
-then
-  task_dir="ramp";
-fi
-if [[ $scene_name == xray_* ]]
-then
-  task_dir="solidity";
-fi
-if [[ $scene_name == yankee_* ]]
-then
-  task_dir="spatial_elimination";
-fi
-if [[ $scene_name == zulu_* ]]
-then
-  task_dir="support_relation";
-fi
-if [[ $scene_name == omega_* ]]
-then
-  task_dir="tool_scene";
-fi
-if [[ $task_dir == "" ]]
-then
-  echo "Cannot run NYU red team (formerly baseline) on scene $scene_name because task is unsupported"
-  exit
-fi
-eval_dir=${eval_dir}/${task_dir}
+#task_dir=""
+#if [[ $scene_name == sierra_* ]]
+#then
+#  task_dir="agent_id";
+#fi
+#if [[ $scene_name == tango_* ]]
+#then
+#  task_dir="moving_target";
+#fi
+#if [[ $scene_name == uniform_* ]]
+#then
+#  task_dir="lava";
+#fi
+#if [[ $scene_name == victor_* ]]
+#then
+#  task_dir="lava";
+#fi
+#if [[ $scene_name == whiskey_* ]]
+#then
+#  task_dir="ramp";
+#fi
+#if [[ $scene_name == xray_* ]]
+#then
+#  task_dir="solidity";
+#fi
+#if [[ $scene_name == yankee_* ]]
+#then
+#  task_dir="spatial_elimination";
+#fi
+#if [[ $scene_name == zulu_* ]]
+#then
+#  task_dir="support_relation";
+#fi
+#if [[ $scene_name == omega_* ]]
+#then
+#  task_dir="tool_scene";
+#fi
+#if [[ $task_dir == "" ]]
+#then
+#  echo "Cannot run NYU red team (formerly baseline) on scene $scene_name because task is unsupported"
+#  exit
+#fi
+#eval_dir=${eval_dir}/${task_dir}
 
 # shellcheck disable=SC2154
 echo "Running NYU red team (formerly baseline) with MCS config $mcs_configfile and scene $scene_file with name $scene_name using eval dir $eval_dir"
