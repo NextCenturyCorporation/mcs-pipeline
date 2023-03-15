9346fb22bef02e4c8b643d1a63930c32e130e79a
Thomas Schellenberg <thomas.schellenberg@caci.com>
2023-03-15 15:36:51 -0400

    More Eval 6 updates to the OPICS pipeline

--- deploy_files/opics/ray_script.sh
+++ deploy_files/opics/ray_script.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 set -m
 
-main_optics
+# This is what the "main_optics" command does.
+echo "MCS Pipeline: Running OPICS environment setup..."
+cd /home/ubuntu/ || exit
+headless
+export OPTICS_HOME=~/main_optics
+export PYTHONPATH=$OPTICS_HOME:$OPTICS_HOME/opics_common
+export OPTICS_DATASTORE=ec2b
+cd $OPTICS_HOME || exit
 cd scripts/ || exit
-OPICS_EVAL_DIR=$(pwd)
 
 # Check passed mcs_config and scene file
 # shellcheck source=/dev/null
 source /home/ubuntu/check_passed_variables.sh
-# shellcheck disable=SC2154
-SCENE_DIR="$eval_dir/eval_scene/"
 
 # shellcheck disable=SC2154
-echo "Running OPICS with config $mcs_configfile and scene $scene_file using eval dir $OPICS_EVAL_DIR"
+echo "MCS Pipeline: Running OPICS with MCS config file $mcs_configfile and eval dir $eval_dir and scene file $scene_file"
+
+echo "MCS Pipeline: Clearing History at $eval_dir/SCENE_HISTORY/"
+rm -f "$eval_dir"/SCENE_HISTORY/*
 
 # Start X
 source /home/ubuntu/start_x_server.sh
 
-# Clear out directories
-echo Clearing History at "$OPICS_EVAL_DIR"/SCENE_HISTORY/
-rm -f "$OPICS_EVAL_DIR"/SCENE_HISTORY/*
-echo Clearing "$SCENE_DIR"
-rm -rf "${SCENE_DIR:?}"/*
-
-# Move files to appropriate locations
-echo Making SCENE_DIR="$SCENE_DIR"
-mkdir -p "$SCENE_DIR"
-echo Moving scene_file="$scene_file" to "$SCENE_DIR"
-cp "$scene_file" "$SCENE_DIR"/
-
 export MCS_CONFIG_FILE_PATH=$mcs_configfile
-
-# Run the Performer code
-echo Starting Evaluation:
-python opics_eval6_run_scene.py --scene "$SCENE_DIR"/"$scene_file"
-
+python opics_eval6_run_scene.py --scene "$scene_file"
 unset MCS_CONFIG_FILE_PATH
