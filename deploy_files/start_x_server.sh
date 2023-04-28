fd58ec7576b4bf8e190da8097cb449250e8e20f2
Phi Le <phi.le@caci.com>
2023-04-28 11:36:28 -0400

    changes for eval-6

--- deploy_files/start_x_server.sh
+++ deploy_files/start_x_server.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-
-# Source the environment 
-echo "Starting jax env"
-cd /home/ubuntu/CoraAgent || exit
-source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
-conda activate jax
-
-
-if pgrep -f "python /home/ubuntu/jax3dp3/scripts/run_physics_server.py" > /dev/null
-then
-  echo 'Physics server is running'
-else
-  echo "Starting run_physics_server.py"
-  # Need to redirect logs of background tasks or the script doesn't
-  python /home/ubuntu/jax3dp3/scripts/run_physics_server.py 1>physserver-out.txt 2>physserver-err.txt &
-  echo "Sleeping for 20 seconds to wait for physics server"
-  sleep 20
-fi
-
-
 # Common code to determine if the X server is running and start it if it is not.
 if pgrep -x Xorg > /dev/null
 then
   echo 'X Server is running'
 else
   echo "Starting X Server"
-  # FOR CORA - they modify /etc/X11/xorg.conf, so dont run xconfig
-  # Need to redirect logs of background tasks or the script doesn't
-  sudo /usr/bin/Xorg :0 -config /etc/X11/xorg.conf 1>startx-out.txt 2>startx-err.txt &
+  sudo nvidia-xconfig --use-display-device=None --virtual=1280x1024 --output-xconfig=/etc/X11/xorg.conf --busid=PCI:0:30:0
+  sudo /usr/bin/Xorg :0 1>startx-out.txt 2>startx-err.txt &
   echo "Sleeping for 20 seconds to wait for X server"
   sleep 20
   echo "Sleep finished"
@@ -49,31 +28,3 @@ else
     fi
   fi
 fi
-
-
-
-# if pgrep -f "python server.py" > /dev/null
-# then
-#   echo 'Vision server is running'
-# else
-#   echo "Starting server.py"
-#   cd /home/ubuntu/jax3dp3/experiments/multiprocess || exit
-#   # Need to redirect logs of background tasks or the script doesn't
-#   python server.py 1 >pyserver-out.txt 2>pyserver-err.txt &
-#   echo "Sleeping for 20 seconds to wait for vision server"
-#   sleep 20
-# fi
-
-# if pgrep -f "python physics_server.py" > /dev/null
-# then
-#   echo 'Physics server is running'
-# else
-#   echo "Starting physics_server.py"
-#   cd /home/ubuntu/CoraAgent/src/MCS/Physics || exit
-#   # Need to redirect logs of background tasks or the script doesn't
-#   python physics_server.py 1 >physserver-out.txt 2>physserver-err.txt &
-#   echo "Sleeping for 20 seconds to wait for physics server"
-#   sleep 20
-# fi
-
-
